3aab0591f8c989b555e3750c9efdee62b0120534
Sayed Razibur Rahman <sayedrazibur@gmail.com>
2025-01-12 06:05:19 +0600

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+        pariatur.
+      </p>
+    </div>
+  );
+};
+
+export default App;
